7921bb78d850ae9c44afb67a80a0516cf2595558
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2025-03-11 16:00:54 -0300

    Back-end iniciado

---
+++ back/src/main/java/br/com/goldform/GoldformApplication.java
@@ -0,0 +1,13 @@
+package br.com.goldform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldformApplication.class, args);
+	}
+
+}
